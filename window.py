b8c17640faf78f8d5419bb8e309843a04f12df12
Maria <kiseleva131415@mail.ru>
2023-02-04 16:02:29 +0300

    update window

--- window.py
+++ window.py
@@ -1,7 +1,8 @@
+from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import (
     QMainWindow,
 )
-from PyQt5.QtGui import QPixmap
+from PyQt5.QtGui import QPixmap, QKeyEvent
 
 from static_maps_api import get_map
 from window_ui import Ui_MainWindow
@@ -19,15 +20,25 @@ class MainWindow(QMainWindow, Ui_MainWindow):
     def _init_ui(self) -> None:
         self._refresh_map()
 
+    def keyPressEvent(self, event: QKeyEvent) -> None:
+        previous_zoom = self._map_zoom
+        if event.key() == Qt.Key_PageUp:
+            self._map_zoom += 1
+        elif event.key() == Qt.Key_PageDown:
+            self._map_zoom -= 1
+        self._map_zoom = self._clip_zoom(self._map_zoom, 1, 16)
+        if previous_zoom != self._map_zoom:
+            self._refresh_map()
+
+    def _clip_zoom(self, zoom: int, min_zoom: int, max_zoom: int) -> int:
+        if zoom < min_zoom:
+            return min_zoom
+        if zoom > max_zoom:
+            return max_zoom
+        return zoom
+
     def _refresh_map(self) -> None:
         image_data = get_map(ll=self._map_ll, zoom=self._map_zoom, map_type=self._map_l)
-        # создание временного файла (плохой способ)
-        # with NamedTemporaryFile('wb', prefix='qt-image-', suffix='.png') as file:
-        #     file.write(image_data)
-        #     file.flush()
-        #     pixmap = QPixmap(file.name)
-        #     self.map_label.setPixmap(pixmap)
         pixmap = QPixmap()
         pixmap.loadFromData(image_data)
         self.map_label.setPixmap(pixmap)
-
