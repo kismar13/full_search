e164df3ad18dcfe329c0a5ff16eed3788ae42127
Maria <kiseleva131415@mail.ru>
2023-02-04 15:43:27 +0300

    create compile window

---
+++ compile_window.py
@@ -0,0 +1,3 @@
+from os import system
+
+system('pyuic5 window.ui -o window_ui.py')
--- main.py
+++ main.py
@@ -1,50 +1,33 @@
-from sys import argv
-from textwrap import dedent
-
-from search_maps import find_nearest_organization
-from geocoder_api import (
-    get_coordinates,
+from PyQt5.QtWidgets import (
+    QMainWindow,
 )
-from show_map import show_map
-from find_distance import find_distance
+from PyQt5.QtGui import QPixmap
+
+from static_maps_api import get_map
+from window_ui import Ui_MainWindow
 
 
-def main() -> None:
-    toponym_to_find = ' '.join(argv[1:])
+class MainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self) -> None:
+        super().__init__()
+        self.setupUi(self)
+        self._map_zoom = 5
+        self._map_ll = 37.977751, 55.757718
+        self._map_l = 'map'
+        self._init_ui()
 
-    if toponym_to_find == '':
-        print('No data')
-        return
+    def _init_ui(self) -> None:
+        self._refresh_map()
 
-    # Получаем координаты ближайшей аптеки.
-    lat, lon = get_coordinates(toponym_to_find)
-    organization = find_nearest_organization(ll=(lat, lon), span=(0.005, 0.005),
-                                             organization_type='аптека')
-    org_lat, org_lon = map(float, organization['geometry']['coordinates'])
-    show_map(ll=(lat, lon), spn=(0.005, 0.005), map_type='map', pt=(org_lat, org_lon, 'pm2dgl'))
+    def _refresh_map(self) -> None:
+        image_data = get_map(ll=self._map_ll, zoom=self._map_zoom, map_type=self._map_l)
+        # создание временного файла (плохой способ)
+        # with NamedTemporaryFile('wb', prefix='qt-image-', suffix='.png') as file:
+        #     file.write(image_data)
+        #     file.flush()
+        #     pixmap = QPixmap(file.name)
+        #     self.map_label.setPixmap(pixmap)
+        pixmap = QPixmap()
+        pixmap.loadFromData(image_data)
+        self.map_label.setPixmap(pixmap)
 
-    # Добавляем на карту точку с исходным адресом.
-    show_map(ll=(lat, lon), spn=(0.005, 0.005), map_type='map',
-             pt=f'{org_lat},{org_lon},pm2dgl~{lat},{lon},pm2rdl')
-
-    # Автопозиционирование
-    show_map(spn=(0.005, 0.005), map_type='map',
-             pt=f'{org_lat},{org_lon},pm2dgl~{lat},{lon},pm2rdl')
-
-    metadata = organization['properties']['CompanyMetaData']
-    # Название организации.
-    name = metadata['name']
-    # Адрес организации.
-    address = metadata['address']
-    # Время работы
-    time = metadata['Hours']['text']
-    # Расстояние
-    distance = round(find_distance((lon, lat), (org_lon, org_lat)))
-    print(dedent(f'''\
-        Название:\t{name}
-        Адрес:\t{address}
-        Время работы:\t{time}
-        Расстояние:\t{distance}м.'''))
-
-
-main()
