b2d24bdc906df1e1018443a73679c020ce597f16
Maria <kiseleva131415@mail.ru>
2023-02-04 15:06:13 +0300

    update geocoder

--- geocoder_api.py
+++ geocoder_api.py
@@ -1,5 +1,7 @@
 import requests
 
+from config import GEOCODER_API_KEY
+
 
 def get_coordinates(address: str):
     toponym = geocoder_request(address)
@@ -35,13 +37,10 @@ def get_coordinates_and_span(address: str):
     return (toponym_longitude, toponym_lattitude), (dx, dy)
 
 
-API_KEY = '40d1649f-0493-4b70-98ba-98533de7710b'
-
-
 def geocoder_request(address: str):
     # Выполняем запрос.
     response = requests.get('http://geocode-maps.yandex.ru/1.x/', params={
-        'apikey': API_KEY,
+        'apikey': GEOCODER_API_KEY,
         'geocode': address,
         'format': 'json',
     })
