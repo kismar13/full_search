7abb706e6b1662513a6f9c7dad701bd965384bff
Maria <kiseleva131415@mail.ru>
2023-02-04 15:44:05 +0300

    update decoder

--- geocoder_api.py
+++ geocoder_api.py
@@ -1,49 +1,22 @@
 import requests
 
-from config import GEOCODER_API_KEY
 
-
-def get_coordinates(address: str):
-    toponym = geocoder_request(address)
-    if toponym is None:
-        return None, None
-
-    # Координаты центра топонима:
-    toponym_coodrinates = toponym['Point']['pos']
-    # Широта, преобразованная в плавающее число:
-    toponym_longitude, toponym_lattitude = map(float, toponym_coodrinates.split(' '))
-    return toponym_longitude, toponym_lattitude
-
-
-def get_coordinates_and_span(address: str):
-    toponym = geocoder_request(address)
-    if toponym is None:
-        return (None, None), (None, None)
-
-    # Координаты центра топонима:
-    toponym_coodrinates = toponym['Point']['pos']
-    # Широта, преобразованная в плавающее число:
-    toponym_longitude, toponym_lattitude = map(float, toponym_coodrinates.split(' '))
-
-    # Рамка вокруг объекта:
-    envelope = toponym['boundedBy']['Envelope']
-    # левая, нижняя, правая и верхняя границы из координат углов:
-    l, b = map(float, envelope['lowerCorner'].split())
-    r, t = map(float, envelope['upperCorner'].split())
-    # Вычисляем полуразмеры по вертикали и горизонтали
-    dx = abs(l - r) / 2
-    dy = abs(t - b) / 2
-
-    return (toponym_longitude, toponym_lattitude), (dx, dy)
-
-
-def geocoder_request(address: str):
-    # Выполняем запрос.
-    response = requests.get('http://geocode-maps.yandex.ru/1.x/', params={
-        'apikey': GEOCODER_API_KEY,
-        'geocode': address,
-        'format': 'json',
-    })
+def get_map(*, ll: tuple[float, float] = None, spn: tuple[float, float] = None,
+            zoom: float = None,
+            map_type: str,
+            pt) -> bytes:
+    params = {
+        'l': map_type,
+    }
+    if ll is not None:
+        params['ll'] = ','.join(map(str, ll))
+    if spn is not None:
+        params['spn'] = ','.join(map(str, spn)),
+    if zoom is not None:
+        params['z'] = zoom
+    if pt is not None:
+        params['pt'] = ','.join(map(str, pt)) if isinstance(pt, tuple) else pt
+    response = requests.get('http://static-maps.yandex.ru/1.x/', params=params)
 
     if not response:
         raise RuntimeError(
@@ -51,9 +24,4 @@ def geocoder_request(address: str):
             {response.request.url}
             Http статус: {response.status_code} ({response.reason})''')
 
-    # Преобразуем ответ в json-объект
-    data = response.json()
-    # Получаем первый топоним из ответа геокодера.
-    # Согласно описанию ответа он находится по следующему пути:
-    features = data['response']['GeoObjectCollection']['featureMember']
-    return features[0]['GeoObject'] if features else None
+    return response.content
